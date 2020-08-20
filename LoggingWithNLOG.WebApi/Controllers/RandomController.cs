499662c7f7a2d50bfa48ac0d6cac3e67ebe1230e
Mukesh Murugan <iammukeshm@gmail.com>
2020-08-20 21:04:27 +0530

    configured NLog

---
+++ LoggingWithNLOG.WebApi/Controllers/RandomController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Fluent;
+
+namespace LoggingWithNLOG.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomController : ControllerBase
+    {
+        private static ILogger<RandomController> _logger;
+        public RandomController(ILogger<RandomController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public void Get()
+        {
+            _logger.LogInformation("Requested a Random API");
+            int count;
+            try
+            {
+                for (count = 0; count <= 5; count++)
+                {
+                    if (count == 3)
+                    {
+                        throw new Exception("Random Exception Occured");
+                    }
+                    else
+                    {
+                        _logger.LogInformation("Iteration Count is {iteration}", count);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex, "Exception Caught");
+            }
+        }
+    }
+}
--- LoggingWithNLOG.WebApi/Program.cs
+++ LoggingWithNLOG.WebApi/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using NLog.Web;
 
 namespace LoggingWithNLOG.WebApi
 {
@@ -13,7 +15,23 @@ namespace LoggingWithNLOG.WebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("NLog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("Application Starting Up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                //NLog: catch setup errors
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -21,6 +39,12 @@ namespace LoggingWithNLOG.WebApi
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                })
+                .UseNLog();  // NLog: Setup NLog for Dependency injection
     }
 }
